9628518cf41badb4919cc628075cba762e399a18
Zafar <ahmadzafar100@gmail.com>
2026-04-19 23:31:29 +0530

    sweet alert bug fix

--- src/ShowUsers.js
+++ src/ShowUsers.js
@@ -52,6 +52,7 @@ function ShowUsers(props) {
       showCancelButton: true,
       confirmButtonColor: "#3085d6",
       cancelButtonColor: "#d33",
+      allowOutsideClick: false,
       confirmButtonText: "Yes, delete it!",
     }).then(async (result) => {
       if (result.isConfirmed) {
